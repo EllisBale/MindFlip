f6fadd4a806125c4c6b830174ab25cc6b57662e4
EllisBale <elscar29@gmail.com>
2025-04-19 16:51:02 +0100

    Add grid and fixed it by changing variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-const images = ["images\fox.jpg","images\fox.jpg","images\fox.jpg","images\fox.jpg","images\fox.jpg","","","","","","","" ]; 
+const images = ["images/fox.jpg","images/fox.jpg","images\fox.jpg","images\fox.jpg","images\fox.jpg","","","","","","","" ]; 
 let level = 1;
 let flippedCards = [];
 let matchedPairs = 0;
@@ -17,8 +17,8 @@ const nextLevelButton = document.getElementById("next-level"); // Next level but
 function startLevel() {
 
     gameBoard.innerHTML = "";
-    resultText.innerText = "";
-    nextLevel.classList.add("hidden");
+    result.innerText = "";
+    nextLevelButton.classList.add("hidden");
 
     // This makes it reset back to 1 when 6 is completed
     if (level > 6) {
@@ -28,6 +28,28 @@ function startLevel() {
     // Set's the grid size depending on what level it is on
     let gridSize = Math.min(level + 1, 6); // Math.min returns the smallest of the numbers
     gameBoard.style.gridTemplateColumns = `repeat(${gridSize}, 100px)`;
+
+    let numPair = (gridSize * gridSize) / 2;
+    let selectedImages = images.slice(0, numPair);
+    let cards = [...selectedImages, ...selectedImages];
+
+    shuffle(cards);
+    flippedCards = [];
+    matchedPairs = 0;
+
+
+
+
+    // Creates the game board
+
+    cards.forEach((imagePath, index) => { // Calls a function for each item
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = "?";
+        card.addEventListener("click", () => flipcard(card));
+        gameBoard.appendChild(card);
+    })
+
 
     
 }
