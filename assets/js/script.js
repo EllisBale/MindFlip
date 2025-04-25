8ddaccecfa6722c1a4dd781690c5187c472914ec
EllisBale <elscar29@gmail.com>
2025-04-25 16:12:21 +0100

    Add how to play functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,22 @@ const modal = document.getElementById("myModal");
 const trigger = document.getElementById("howtoplay");
 const span = document.getElementsByClassName("close")[0];
 
+trigger.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target ==  modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
 
 // Functions
 
